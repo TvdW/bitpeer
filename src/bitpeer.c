d14f37a079c1cb9189afdd12d92ebfaaaae4ab90
Tom van der Woerdt <info@tvdw.eu>
2013-04-14 19:49:57 +0200

    A better CLI interface

--- src/bitpeer.c
+++ src/bitpeer.c
@@ -71,50 +71,103 @@ int main(int argc, char** argv)
 	program.addrpool_size = 10240;
 	program.min_connections = 32;
 	program.max_connections = 4096;
-	//program.reindex_blocks = 1;
+	program.reindex_blocks = 0;
 	program.relay_transactions = 1;
 	program.relay_blocks = 0;
 	program.txpool_size = 1024;
 	
 	/* Interpret the command line */
-	if (argc < 4) {
+	if (argc < 2) {
 		printf("Invalid command line arguments.\n");
-		printf("Syntax:     bitpeer [listen_port] [local_addr:port] [seed_addr:port]\n");
+		printf("Usage:  bitpeer [listen_port] [public_ip] -n [seed_node]\n");
+		printf("public_ip and seed_node may optionally contain a port number.\n");
 		return EXIT_FAILURE;
 	}
 	
 	unsigned short listen_v4 = atoi(argv[1]);
 	if (listen_v4 == 0) {
-		printf("Invalid port specified\n");
+		printf("Invalid port number specified. Valid ports go from 1 to 65535.\n");
 		return EXIT_FAILURE;
 	}
 	
 	struct sockaddr v4_sockaddr;
+	struct sockaddr_in *v4_sockaddr_p = (struct sockaddr_in*)&v4_sockaddr;
 	int v4_sockaddr_len = sizeof(v4_sockaddr);
 	if (evutil_parse_sockaddr_port(argv[2], &v4_sockaddr, &v4_sockaddr_len) < 0) {
-		printf("Invalid local_addr specified\n");
+		printf("Invalid public_ip specified\n");
 		return EXIT_FAILURE;
 	}
 	if (v4_sockaddr.sa_family != AF_INET) {
-		printf("local_addr must be IPv4\n");
+		printf("public_ip must be IPv4\n");
 		return EXIT_FAILURE;
 	}
-	struct sockaddr_in *v4_sockaddr_p = (struct sockaddr_in*)&v4_sockaddr;
 	if (v4_sockaddr_p->sin_port == 0) {
-		
-		printf("No local_addr port specified\n");
-		return EXIT_FAILURE;
+		v4_sockaddr_p->sin_port = ntohs(listen_v4);
 	}
 	
-	struct sockaddr seed_sockaddr;
-	int seed_sockaddr_len = sizeof(seed_sockaddr);
-	if (evutil_parse_sockaddr_port(argv[3], &seed_sockaddr, &seed_sockaddr_len) < 0) {
-		printf("Invalid seed_addr specified\n");
-		return EXIT_FAILURE;
-	}
+	int nodecount = 0;
+	struct sockaddr *nodeaddrs = NULL;
+	int *nodelens = NULL;
 	
-	if (argc >= 5) {
-		program.min_connections = atoi(argv[4]);
+	/* Now interpret the optional arguments */
+	// Quick preprocessor macro to go to the next argv
+	
+#define NEXT_I() {i++; if (i >= argc) { printf("Argument for %s missing\n", argv[i-1]); return EXIT_FAILURE; }}
+	for (int i = 2; i < argc; i++) {
+		if (strcmp(argv[i], "--reindex") == 0) {
+			program.reindex_blocks = 1;
+		}
+		else if (strcmp(argv[i], "--addnode") == 0 || strcmp(argv[i], "-n") == 0) {
+			NEXT_I();
+			nodeaddrs = realloc(nodeaddrs, (nodecount+1) * sizeof(struct sockaddr));
+			nodelens = realloc(nodelens, (nodecount+1) * sizeof(int));
+			nodelens[nodecount] = sizeof(struct sockaddr);
+			struct sockaddr *addr = &nodeaddrs[nodecount];
+			memset(addr, 0, sizeof(struct sockaddr));
+			
+			if (evutil_parse_sockaddr_port(argv[i], addr, nodelens+nodecount) < 0) {
+				printf("Invalid node address specified: %s %d\n", argv[i], nodelens[nodecount]);
+				return EXIT_FAILURE;
+			}
+			
+			// TODO: port
+			
+			nodecount += 1;
+		}
+		else if (strcmp(argv[i], "--txpool") == 0) {
+			NEXT_I();
+			program.txpool_size = atoi(argv[i]);
+			if (program.txpool_size <= 0) {
+				printf("Invalid argument for %s\n", argv[i-1]);
+				return EXIT_FAILURE;
+			}
+		}
+		else if (strcmp(argv[i], "--addrpool") == 0) {
+			NEXT_I();
+			program.addrpool_size = atoi(argv[i]);
+			if (program.addrpool_size <= 0) {
+				printf("Invalid argument for %s\n", argv[i-1]);
+				return EXIT_FAILURE;
+			}
+		}
+		else if (strcmp(argv[i], "--no-tx") == 0) {
+			program.relay_transactions = 0;
+		}
+		else if (strcmp(argv[i], "--no-blocks") == 0) {
+			program.relay_blocks = 0;
+		}
+		else if (strcmp(argv[i], "--minconn") == 0) {
+			NEXT_I();
+			program.min_connections = atoi(argv[i]);
+		}
+		else if (strcmp(argv[i], "--maxconn") == 0) {
+			NEXT_I();
+			program.max_connections = atoi(argv[i]);
+			if (program.max_connections <= 0) {
+				printf("Invalid argument for %s\n", argv[i-1]);
+				return EXIT_FAILURE;
+			}
+		}
 	}
 	
 	/* Create the main loop */
@@ -138,9 +191,14 @@ int main(int argc, char** argv)
 	
 	/* We need to connect to one initial node in order to seed everything.
 	   We will not do this initial discovery ourselves. */
-	bp_connection_s *seed_connection = malloc(sizeof(bp_connection_s));
-	bp_connection_connect(seed_connection, &server, &seed_sockaddr, seed_sockaddr_len);
-	seed_connection->is_seed = 1;
+	for (int i = 0; i < nodecount; i++) {
+		bp_connection_s *seed_connection = malloc(sizeof(bp_connection_s));
+		bp_connection_connect(seed_connection, &server, &nodeaddrs[i], nodelens[i]);
+		seed_connection->is_seed = 1;
+	}
+	
+	free(nodeaddrs);
+	free(nodelens);
 	
 	/* Run the loop */
 	event_base_loop(program.eventbase, 0);
